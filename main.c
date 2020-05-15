053c570a0ea18005aa4e5625ce9de74eed861120
drew6017 <drew6017@gmail.com>
2020-05-15 00:33:52 -0500

    added argument parsing with argp, changed a lot of internal structure

--- main.c
+++ main.c
@@ -9,30 +9,77 @@
 #include <malloc.h>
 #include <string.h>
 #include <unistd.h>
+#include <argp.h>
 
 #include "fifo.h"
 
-#define FIFO_SIZE 512
-#define MAX_OPEN_DESCRIPTORS 15
-
 typedef struct {
-    fifo_t* fifo;
-    size_t directoryTrim;
-    char query[64];
-} search_context_t;
+    char* query;
+    int fifoSize;
+    int maxFileDesc;
+    int directoryTrim;
+    long threads;
+    char* directory;
+} arguments_t;
+
+const char* argp_program_version     = "fastgrep 1.1";
+//const char* argp_program_bug_address = "<contact@divisionind.com>";
+const char program_desc[]            = "Searches for files recursively in a [-d directory] for the ASCII sequence [QUERY].";
+const char program_usage[]           = "[QUERY]";
+static struct argp_option options[] = {
+    {"buffer-size", 's', "SIZE",     0, "Number of file paths to allow as a buffer for consumption by the worker threads"},
+    {"file-desc",   'f', "NDESC",    0, "Max open file desc (only for path traversal), the true usage is [N-(worker threads)]"},
+    {"trim-paths",  'p', 0,          0, "Do NOT trim the file paths with the current dir"},
+    {"threads",     't', "NTHREADS", 0, "Number of threads to use for scanning, default is [(available processors) - 1]"},
+    {"directory",   'd', "DIR",      0, "Directory to scan"}
+};
+
+error_t parse_opt(int key, char* in, struct argp_state* state) {
+    arguments_t* arg = (arguments_t*) state->input;
+    switch (key) {
+        case 's':
+            arg->fifoSize = atoi(in);
+            break;
+        case 'f':
+            arg->maxFileDesc = atoi(in);
+            break;
+        case 'p':
+            arg->directoryTrim = 0;
+            break;
+        case 't':
+            arg->threads = atol(in);
+            break;
+        case 'd':
+            arg->directory = in;
+            break;
+        case ARGP_KEY_ARG:
+            if (state->arg_num >= 1)
+                argp_usage(state);
+            else
+            if (state->arg_num == 0)
+                arg->query = in;
+            break;
+        case ARGP_KEY_END:
+            if (state->arg_num < 1)
+                argp_usage(state);
+            break;
+        default:
+            return ARGP_ERR_UNKNOWN;
+    }
+    return 0;
+}
 
 // i hate using a global here but nftw has no way of passing data
 // maybe implement a similar version? check speed
 fifo_t g_fifo;
 
 void* task_search(void* arg) {
-    search_context_t* context = arg;
-    fifo_t* fifo = context->fifo;
+    arguments_t * context = arg;
     const char* query = context->query;
     char* filename;
 
-    while (!fifo->closed) {
-        if (fifo_get(fifo, &filename)) {
+    while (!g_fifo.closed) {
+        if (fifo_get(&g_fifo, &filename)) {
             continue;
         }
         FILE* file = fopen(filename, "r");
@@ -76,60 +123,58 @@ int task_load_file_entry(const char *filename, const struct stat *info, int flag
 
 /*
  * Create options for:
- * - directory trimming
- * - fifo size
- * - max open file desc
- * - search threads
  * - ignore case / regex
+ * - disable color   (impl color first)
+ * - disable preview (impl prev first)
  */
 int main(int argc, char* argv[]) {
-    // TODO impl cmd line parsing using getopt (http://man7.org/linux/man-pages/man3/getopt.3.html)
-    if (argc < 2) {
-        printf("usage: fastgrep <query> <none:dir>\n");
+    arguments_t args;
+    memset(&args, 0, sizeof(args));
+    args.fifoSize      = 512;
+    args.maxFileDesc   = 15;
+    args.threads       = sysconf(_SC_NPROCESSORS_ONLN) - 1;
+    args.directory     = ".";
+    args.directoryTrim = -1;
+
+    struct argp arg_parser = {options, parse_opt, program_usage, program_desc};
+    argp_parse(&arg_parser, argc, argv, 0, 0, &args);
+
+    // resolve directory
+    args.directory = realpath(args.directory, NULL);
+    if (args.directory == NULL) {
+        fprintf(stderr, "invalid directory\n");
         return 1;
     }
 
-    if (fifo_create(&g_fifo, FIFO_SIZE, sizeof(char*))) {
+    if (args.directoryTrim == -1)
+        args.directoryTrim = (int) strlen(args.directory) + 1;
+
+    // done parsing args, create fifo
+    if (fifo_create(&g_fifo, args.fifoSize, sizeof(char*))) {
         fprintf(stderr, "insufficient memory or other resources\n");
         return 1;
     }
 
-    // resolve dir from args
-    char* potentialDir = argc >= 3 ? argv[2] : ".";
-    char* dir = realpath(potentialDir, NULL);
-    if (dir == NULL) {
-        fprintf(stderr, "invalid directory: %s\n", potentialDir);
-        return 1;
-    }
-
-    long searchThreads = sysconf(_SC_NPROCESSORS_ONLN) - 1;
-    if (searchThreads < 1) {
+    if (args.threads < 1) {
         fprintf(stderr, "invalid processor configuration\n");
         return 1;
     }
 
-    printf("Searching \"%s\"...\n", dir);
-
-    // create a context to pass to threads
-    search_context_t searchContext;
-    searchContext.fifo = &g_fifo;
-    searchContext.directoryTrim = strlen(dir) + 1;
-//    searchContext.directoryTrim = 0;
-    strcpy(searchContext.query, argv[1]);
+    printf("Searching \"%s\"...\n", args.directory);
 
     // init threads
-    pthread_t* threads = malloc(sizeof(pthread_t) * searchThreads);
-    for (int i = 0; i < searchThreads; i++) {
-        pthread_create(&threads[i], NULL, task_search, &searchContext);
+    pthread_t* threads = malloc(sizeof(pthread_t) * args.threads);
+    for (int i = 0; i < args.threads; i++) {
+        pthread_create(&threads[i], NULL, task_search, &args);
     }
 
     // iterate files and send them to the fifo
     // maybe dont use nftw so I can avoid global usage (see performance)
-    nftw(dir, task_load_file_entry, MAX_OPEN_DESCRIPTORS, FTW_PHYS); // max # open file descriptors, do not follow symlinks
+    nftw(args.directory, task_load_file_entry, args.maxFileDesc, FTW_PHYS); // max # open file descriptors, do not follow symlinks (todo maybe allow this?)
 
     // cleanup
     fifo_close(&g_fifo); // ensure it is closed before joining threads
-    for (int i = 0; i < searchThreads; i++) {
+    for (int i = 0; i < args.threads; i++) {
         pthread_join(threads[i], NULL);
     }
     fifo_free(&g_fifo);
